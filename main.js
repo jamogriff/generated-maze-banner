8f05a3c32809a080247c7d0b34c5e146d7158f48
Jamison Griffith <jamison.griffith@gmail.com>
2021-08-16 21:43:58 -0600

    Create WIP aspectRatio scaling function

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 import "./banner.js";
 import Maze from "./maze.js";
 
-let maze = new Maze(300, 300, 15, 15);
+// create x:1 aspect ratio
+function aspectRatio(width, cellSize, ratio) {
+	// would be preferable to implement promise here
+	if (width < 200 || width > 800) return undefined;
+	else {
+		let height = width / ratio;
+		// Wall rendering was visually tuned to cell size being 10%
+		// of height or width
+		let dimensions = {
+			x: width,
+			y: height,
+			rows: width / cellSize,
+			columns: width / cellSize 
+		};
+		return dimensions;
+	}
+}
+const size = aspectRatio(200, 20, 1);
+
+/*
+create a new maze banner where:
+(width, height, # rows, # columns)
+*/
+let maze = new Maze(size.x, size.y, size.rows, size.columns);
 maze.initialize();
 maze.draw();
