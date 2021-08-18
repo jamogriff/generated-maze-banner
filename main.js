97bf4723f1be3d509d39077971db4b07fbe68bde
Jamison Griffith <jamison.griffith@gmail.com>
2021-08-18 10:47:13 -0600

    Adds logo to DOM with helper functions for canvas placement

--- main.js
+++ main.js
@@ -18,7 +18,32 @@ function aspectRatio(width, cellSize, ratio) {
 	}
 }
 
+function logoPlacement(width, height) {
+	// Using Canvas height to constrain height of logo
+	let yBuffer = 20; // space buffer above and below logo
+	let logoHeight = height - yBuffer;
+	let logoWidth = logoHeight * 2.5; // 2.5:1 is aspect ratio of my logo, YMMV
+	let logoX = width / 2 - logoWidth / 2;
+	let logoY = yBuffer / 2;
+	let logo = {
+		x: logoX,
+		y: logoY,
+		width: logoWidth,
+		height: logoHeight
+	}
+	return logo;
+}
+
+function drawLogo(logo) {
+	let canvas = document.getElementById("banner");
+	let ctx = canvas.getContext("2d");
+	let image = document.getElementById("brand");
+	ctx.drawImage(image, logo.x, logo.y, logo.width, logo.height);
+}
+
+
 const size = aspectRatio(400, 30, 4);
+//const logo = logoPlacement(size.x, size.y);
 
 /*
 create a new maze banner where:
@@ -26,4 +51,5 @@ create a new maze banner where:
 */
 let maze = new Maze(size.x, size.y, size.rows, size.columns);
 maze.initialize();
-maze.draw();
+drawLogo(logoPlacement(size.x, size.y));
+//maze.draw();
